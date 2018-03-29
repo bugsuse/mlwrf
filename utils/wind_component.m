e29eb041a6ed12e206b29d4bbae296800b57b73a
bugsuse <libravo@foxmail.com>
2018-03-29 11:16:02 +0800

    fixed bugs

--- utils/wind_component.m
+++ utils/wind_component.m
@@ -1,26 +1,26 @@
 function uv = wind_component(wspd, wdir)
-% 由 风向和风速 计算 u, v
-% 参数声明：
-%    输入参数： 
-%        wspd  ：  风速.  单位： m/s
-%        wdir  ：  风向.  单位： °
-%    输出参数：
-%        uv  ： 包含 u , v.  单位： m/s
-%  =========================================================
+% 根据风速和风向计算x和y方向水平风
+%% 参数说明:
+%  输入参数:
+%   wspd :  风速. 一维向量.  单位: m/s
+%   wdir :  风向. 一维向量.  单位: 度. 范围应在 0-360
+%  输出参数:
+%    uv : x和y 方向水平风大小
+%      uv(1, :) 表示 u 分量, 单位: m/s
+%      uv(2, :) 表示 v 分量, 单位: m/s
+%% 
 %    date  :  2017.1.8
 %    by    :  ly
 %    email :  libravo@foxmail.com
-%%  参考 NCL 中 wind_component  函数
+%%
 
-rad = vpa(0.17452925199433);
-uvmsg = 1e20;
 uveps = 1e-5;
 uvzero = 0.0;
 
-u = -wspd*sin(wdir*rad);
-v = -wspd*cos(wdir*rad);
+u = -wspd.*sin(wdir*pi/180);
+v = -wspd.*cos(wdir*pi/180);
 
-uv = [u;v];
-uv(uv <= uveps) = uvzero;
+uv = [u; v];
+uv(abs(uv) <= uveps) = uvzero;
 
 end
--- utils/wind_direction.m
+++ utils/wind_direction.m
@@ -1,27 +1,29 @@
 function wdir = wind_direction(u, v, fillvalue)
-%  根据 u, v 计算 wspd, wdir
-%  参数声明：
-%     输入参数： 
-%        u,v    ： 均为风速. 单位 : m/s.
-%     fillvalue ： 缺省值设置.  默认为 0.
-%                可为 标量 或 NaN
-%     输出参数：
-%        wdir  ：  风向. 
-%  ================================================================
+%  根据 u, v 分量计算风向
+%% 参数说明:
+%    输入参数:
+%      u  :  风速u分量. 一维数值向量.  单位: m/s
+%      v  :  风速v分量. 一维数值向量.  单位: m/s
+%         u 和 v向量大小相同.
+%   fillvalue : 用于填充无效值. 标量. 
+%     输出参数:
+%        wdir : 风向.  一维向量. 大小和 u, v 大小相同. 单位: 度. 正北方向为0度. 
+%% 
 %    date  :  2017.1.8
 %    by    :  ly
 %    email :  libravo@foxmail.com
-%%  参考 NCL 中 wind_direction 函数
-% 常数定义
+%%
+
 if nargin == 2
     fillvalue = 0;
 end
+
 zero = 0;
 con = 180.0;
 wcrit = 360 - 0.00002;
 radi = 1.0/0.01745329;
 
 wdir = atan2(u, v)*radi + con;
-wdir(wdir >= wcrit) = zero;  % force 360 "north winds"
+wdir(wdir >= wcrit) = zero;  % 强制大于 360 的为正北方向
 wdir(u == 0 & v == 0) = fillvalue; 
 end
